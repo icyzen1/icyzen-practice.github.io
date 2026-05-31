1ae0e2c54ddd8f91787fa3d55bc378332d469939
icyzen1 <rinkasovskii@gmail.com>
2026-05-31 14:41:58 +0300

    home page developed - mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log("Project started");
+document.addEventListener("DOMContentLoaded", function () {
+    const burgerToggle = document.getElementById("burgerToggle");
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    if (burgerToggle && mobileMenu) {
+        burgerToggle.addEventListener('click', function () {
+            mobileMenu.classList.toggle('active');
+        });
+    }
+});
